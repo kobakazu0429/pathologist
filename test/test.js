1abad8ae7df5af71c145eab3c3611126fdc60d46
kobakakzu0429 <kobakazu10@gmail.com>
2021-01-21 14:20:23 +0900

    fix test

--- test/test.js
+++ test/test.js
@@ -1,21 +1,27 @@
-/*global require, describe, it, __dirname */
-const path = require( 'path' );
-const fs = require( 'fs' );
-const assert = require( 'assert' );
-const pathologist = require( '..' );
+/* eslint-env mocha */
+const path = require("path");
+const fs = require("fs/promises");
+const assert = require("assert");
+const pathologist = require("..");
 
-require( 'source-map-support' ).install();
+require("source-map-support").install();
 
-describe( 'pathologist', () => {
-	const SAMPLES = path.join( __dirname, 'samples' );
-	fs.readdirSync( SAMPLES ).forEach( dir => {
-		( /-SOLO$/.test( dir ) ? it.only : it )( dir, () => {
-			const input = fs.readFileSync( path.join( SAMPLES, dir, 'input.svg' ), 'utf-8' ).trim();
-			const output = fs.readFileSync( path.join( SAMPLES, dir, 'output.svg' ), 'utf-8' ).trim();
-			const paths = JSON.parse( fs.readFileSync( path.join( SAMPLES, dir, 'paths.json' ), 'utf-8' ).trim() );
+describe("pathologist", async () => {
+  const SAMPLES = path.join(__dirname, "samples");
+  const dirs = await fs.readdir(SAMPLES);
+  dirs.forEach((dir) => {
+    (/-SOLO$/.test(dir) ? it.only : it)(dir, async () => {
+      const filenames = ["input.svg", "output.svg", "paths.json"];
+      const [input, output, paths] = (
+        await Promise.all(
+          filenames.map((name) =>
+            fs.readFile(path.join(SAMPLES, dir, name), "utf-8")
+          )
+        )
+      ).map((v) => v.trim());
 
-			assert.equal( pathologist.transform( input ), output );
-			assert.deepEqual( pathologist.parse( input ).paths, paths );
-		});
-	});
+      assert.strictEqual(pathologist.transform(input), output);
+      assert.deepStrictEqual(pathologist.parse(input).paths, JSON.parse(paths));
+    });
+  });
 });
