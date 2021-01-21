77f01b323c27d7cd8883f445438ff638f7a6bd5c
kobakakzu0429 <kobakazu10@gmail.com>
2021-01-21 14:24:08 +0900

    lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,25 @@
-import buble from 'rollup-plugin-buble';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import buble from "rollup-plugin-buble";
+import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from "rollup-plugin-node-resolve";
 
 export default {
-	entry: 'src/index.js',
-	moduleName: 'pathologist',
-	plugins: [
-		buble({
-			transforms: {
-				dangerousForOf: true
-			}
-		}),
-		nodeResolve({
-			module: true,
-			main: true
-		}),
-		commonjs()
-	],
-	sourceMap: true,
-	targets: [
-		{ format: 'es', dest: 'dist/pathologist.es.js' },
-		{ format: 'umd', dest: 'dist/pathologist.umd.js' }
-	]
+  entry: "src/index.js",
+  moduleName: "pathologist",
+  plugins: [
+    buble({
+      transforms: {
+        dangerousForOf: true,
+      },
+    }),
+    nodeResolve({
+      module: true,
+      main: true,
+    }),
+    commonjs(),
+  ],
+  sourceMap: true,
+  targets: [
+    { format: "es", dest: "dist/pathologist.es.js" },
+    { format: "umd", dest: "dist/pathologist.umd.js" },
+  ],
 };
